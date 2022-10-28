8e72a8b4771b6775a544cbb4fe3cfa8b43752fd0
Johnny Magrippis <j@magrippis.com>
2022-10-28 20:00:19 +0200

    ✨  add social image preview on homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import {headers} from 'next/headers'
 import {Montserrat, Fuzzy_Bubbles} from '@next/font/google'
 import './globals.css'
 
@@ -11,18 +12,46 @@ const fancyFont = Fuzzy_Bubbles({
   display: 'swap',
 })
 
-const RootLayout = ({children}: {children: React.ReactNode}) => (
-  <html lang="en" className={`${sansSerifFont.variable} ${fancyFont.variable}`}>
-    <head>
-      <title>Browsercat</title>
-      <meta
-        name="description"
-        content="A Social Preview Image generation service, built on Next.js"
-      />
-      <link rel="icon" href="/favicon.ico" />
-    </head>
-    <body>{children}</body>
-  </html>
-)
+const RootLayout = ({children}: {children: React.ReactNode}) => {
+  const headersList = headers()
+  const host = headersList.get('host')
+  const rootPath =
+    host && !host.includes('localhost')
+      ? `https://${host}`
+      : 'http://localhost:3000'
+
+  const title = 'Browsercat'
+  const description =
+    'A Social Preview Image generation service, built on Next.js'
+
+  const image = {
+    url: `${rootPath}/api/og?title=Browsercat%F0%9F%90%88%E2%80%8D%E2%AC%9B&subtitle=Professional%20Social%20Preview%20images,%20for%20everyone%20%F0%9F%98%B8`,
+    alt: 'Browsercat is PROFESSIONAL Social Preview images, for everyone!',
+  }
+
+  return (
+    <html
+      lang="en"
+      className={`${sansSerifFont.variable} ${fancyFont.variable}`}
+    >
+      <head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="article" />
+
+        <meta property="og:image" content={image.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content="Browsercat" />
+        <meta name="twitter:image:alt" content={image.alt} />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
 
 export default RootLayout
